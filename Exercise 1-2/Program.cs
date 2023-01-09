d92e9083b679714cd2b7420c702531522b7bb053
AyotundeA <121546620+AyotundeA@users.noreply.github.com>
2023-01-09 08:01:36 +0000

    Create Program.cs

---
+++ Exercise 1-2/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Exercise_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EXERCISE 1 - 2: Bonnie Tyler
+            //Let’s write some more. Bonnie Tyler once sang about feelings. Create a program using 3 Console.WriteLine’s that print these lyrics.
+
+
+            Console.WriteLine("Once upon a time ");
+            Console.WriteLine("I was falling in love ");
+            Console.WriteLine("Now I'm only falling apart ");
+            Console.ReadLine();
+        }
+    }
+}
