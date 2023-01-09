54fa602d26229cfce028f3511a609454c33cd092
AyotundeA <121546620+AyotundeA@users.noreply.github.com>
2023-01-09 08:04:54 +0000

    Create Program.cs

---
+++ Exercise 1-5/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Exercise_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Modify the code so that it prints Hello Ada LoveLace! Do not modify the variable content!
+            string name = "Ada Lovelace";
+            Console.WriteLine(name);
+        }
+    }
+}
