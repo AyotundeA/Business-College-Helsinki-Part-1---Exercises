a374696ba2fb31d701ee7cfbf81dee12072a6ef0
AyotundeA <121546620+AyotundeA@users.noreply.github.com>
2023-01-09 08:03:59 +0000

    Create Program.cs

---
+++ Exercise 1-4/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Exercise_1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Change it so that it prints Passport and a toothbrush! Do not change the line containing Console.WriteLine. Only change the content of the variable!
+            string message = "Passport and toothbrush!";
+            Console.WriteLine(message);
+        }
+    }
+}
