854bca7617cff78f96f4ad6eac0030ca3773afa9
AyotundeA <121546620+AyotundeA@users.noreply.github.com>
2023-01-09 08:02:18 +0000

    Create Program.cs

---
+++ Exercise 1-3/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Exercise_1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EXERCISE 1-3: Bonnie Tyler Line Change
+            //Let’s refine our program. Print the same message. This time use only one Console.WriteLine method.
+
+            Console.WriteLine("Once upon a time \nI was falling in love \nNow I'm only falling apart ");
+            Console.ReadLine();
+        }
+    }
+}
