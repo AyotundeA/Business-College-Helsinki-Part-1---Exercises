2d9f93d4182af80e80fa66199993d38302cdf1dc
AyotundeA <121546620+AyotundeA@users.noreply.github.com>
2023-01-11 09:03:26 +0000

    Create Program.cs

---
+++ Exercise 1-12/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Exercise_1_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 11;
+            int number2 = 42;
+            Console.WriteLine("Give a number!");
+            Console.WriteLine(">" + number1);
+            Console.WriteLine("You gave " + number1);
+            Console.WriteLine("Give a number!");
+            Console.WriteLine(">" + number2);
+            Console.WriteLine("You gave " + number2);
+        }
+    }
+}
