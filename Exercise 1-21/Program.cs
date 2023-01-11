8750a3f75f884676e78d74ca4cde813fc7c253ed
AyotundeA <121546620+AyotundeA@users.noreply.github.com>
2023-01-11 09:12:11 +0000

    Create Program.cs

---
+++ Exercise 1-21/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+
+
+namespace Exercise_1_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program which asks the user for two integers and calculates their average as a double.
+
+            var arr = new int[] { 3 , 2 };
+            double avg = Queryable.Average(arr.AsQueryable());
+            int number1 = 3;
+            int number2 = 2;
+
+            Console.WriteLine("Give the first number!");
+            Console.WriteLine("> " + number1);
+            Console.WriteLine("Give the second number!");
+            Console.WriteLine("> " + number2);
+            Console.WriteLine("Average = " + avg);
+        }
+    }
+}
