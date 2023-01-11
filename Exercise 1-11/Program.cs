67462e318c4bab3cc4143e914ccd601650f9aec3
AyotundeA <121546620+AyotundeA@users.noreply.github.com>
2023-01-11 09:02:35 +0000

    Create Program.cs

---
+++ Exercise 1-11/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Exercise_1_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daystosummer = 200;
+            double hourstolunch = 3.5;
+            string codingisfun = "It sure is!";
+
+            Console.WriteLine("Days to summer: \n" + daystosummer);
+            Console.WriteLine("Hours to lunch: \n" + hourstolunch);
+            Console.WriteLine("Coding is fun: \n" + codingisfun);
+        }
+    }
+}
