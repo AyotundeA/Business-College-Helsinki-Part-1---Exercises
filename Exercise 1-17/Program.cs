ebf4869cd2c2867e48918237bfac8153b8a77d8b
AyotundeA <121546620+AyotundeA@users.noreply.github.com>
2023-01-11 09:09:48 +0000

    Create Program.cs

---
+++ Exercise 1-17/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Exercise_1_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 8;
+            int number2 = 3;
+            int totalnumber = number1 + number2;
+
+            Console.WriteLine("Give the first number!");
+            Console.WriteLine("> " + number1);
+            Console.WriteLine("Give the second number!");
+            Console.WriteLine("> " + number2);
+            Console.WriteLine("The sum is " + totalnumber);
+        }
+    }
+}
